a8183013ff825cd4f4a41e89639b357818e8af66
wangjieCode <1787612871@qq.com>
2019-11-30 15:55:49 +0800

    添加加载进度条

--- src/main.js
+++ src/main.js
@@ -11,7 +11,17 @@ import './styles.scss'
 
 // 模拟数据入口文件
 import './mock/index'
-
+import NProgress from 'nprogress' // 进度条
+import 'nprogress/nprogress.css' //这个样式必须引入
+NProgress.inc(0.4)
+NProgress.configure({ easing: 'ease', speed: 500, showSpinner: false })
+router.beforeEach((to,from,next) => {
+  NProgress.start()
+  next()
+})
+router.afterEach(() => {
+  NProgress.done()
+})
 // 根据开发环境开启浏览器中的vue开发插件
 if (process.env.NODE_ENV == 'development') {
   Vue.config.devtools = true;
